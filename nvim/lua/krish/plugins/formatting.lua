2d93f70cb55df94aa913aa3e8922b5433022808e
Krish <krishanth5000@gmail.com>
2025-06-09 00:04:24 +0530

    nvim: fixed formatter timeout issue

--- nvim/lua/krish/plugins/formatting.lua
+++ nvim/lua/krish/plugins/formatting.lua
@@ -3,7 +3,6 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local conform = require("conform")
-
 		conform.setup({
 			formatters_by_ft = {
 				css = { "prettier" },
@@ -21,23 +20,52 @@ return {
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 5000,
 			},
-			-- formatters = {
-			-- 	clangformat = {
-			-- 		command = "clang-format",
-			-- 		args = {
-			-- 			"--style=file",
-			-- 		},
-			-- 	},
-			-- },
+			formatters = {
+				black = {
+					command = function()
+						local venv = os.getenv("VIRTUAL_ENV")
+						if venv then
+							return venv .. "/bin/python"
+						end
+						return "python"
+					end,
+					args = { "-m", "black", "--stdin-filename", "$FILENAME", "--quiet", "-" },
+					stdin = true,
+					timeout_ms = 8000,
+					-- Additional fixes for race conditions:
+					exit_codes = { 0 },
+					env = {
+						PYTHONUNBUFFERED = "1",
+					},
+				},
+				isort = {
+					command = function()
+						local venv = os.getenv("VIRTUAL_ENV")
+						if venv then
+							return venv .. "/bin/python"
+						end
+						return "python"
+					end,
+					args = { "-m", "isort", "--stdout", "--filename", "$FILENAME", "-" },
+					stdin = true,
+					timeout_ms = 5000, -- Keep as is, isort is faster
+					-- Additional fixes:
+					exit_codes = { 0 },
+					env = {
+						PYTHONUNBUFFERED = "1",
+					},
+				},
+			},
+			log_level = vim.log.levels.WARN, -- FIXED: Reduced from DEBUG to WARN (less noise)
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 			conform.format({
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 10000, -- FIXED: Increased from 1000ms
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
